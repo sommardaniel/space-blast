7b0cc67a399fe218184c126780a45562a1278b01
Daniel Vaknine <daniel.vaknine@gmail.com>
2024-07-17 22:29:15 +0200

    bullets added

--- main.py
+++ main.py
@@ -15,15 +15,27 @@ class Player:
         self.velocity = velocity
         self.body = pygame.Rect((100, HEIGHT/2, size, size))
 
+
 class Enemy:
     def __init__(self, size, velocity):
         self.size = size
         self.velocity = velocity
         self.body = pygame.Rect((WIDTH, HEIGHT/2, size, size))
 
+class Bullet:
+    def __init__(self, size, velocity, x, y):
+        self.size = size
+        self.velocity = velocity
+        self.body = pygame.Rect((x, y, size, size))
 
 #def spawnEnemy():
-    
+
+bullets = []
+
+def shoot():
+    bullet = Bullet(2, 10, playerOne.body.x + playerOne.size, playerOne.body.y + playerOne.size / 2)
+    bullets.append(bullet)
+
 enemy = Enemy(10, -2)
 playerOne = Player("Daniel", 20, 3)
 
@@ -38,9 +50,13 @@ while running:
     
     enemy.body.move_ip(enemy.velocity, 0)
 
+    for bullet in bullets:
+        bullet.body.move_ip(bullet.velocity, 0)
+        pygame.draw.rect(screen, "orange", bullet.body)
+
     #KEY EVENT
     key = pygame.key.get_pressed()
-
+    
     if key[pygame.K_UP] == True:
         playerOne.body.move_ip(0, playerOne.velocity*(-1))
     elif key[pygame.K_DOWN] == True:
@@ -50,6 +66,8 @@ while running:
     elif key[pygame.K_RIGHT] == True:
         playerOne.body.move_ip(playerOne.velocity, 0)
     
+    if key[pygame.K_SPACE] == True:
+        shoot()
     #GAME EVENTS   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
